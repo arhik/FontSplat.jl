a1264ef2b4401128c9b1e77c02e7155a19a0a5be
Arhik <arhik23@gmail.com>
2024-01-10 17:58:41 +0530

    snapshot

--- src/model.jl
+++ src/model.jl
@@ -5,6 +5,7 @@ using Images
 #using ImageView
 using LinearAlgebra
 using ImageInTerminal
+using Statistics
 
 struct GSplatData
 	means
@@ -14,31 +15,7 @@ struct GSplatData
 	opacities
 end
 
-
 using Rotations
-
-function genSplatData(n)
-	means = rand(2, n)
-	rots = rand(4, n)
-	colors = rand(3, n)
-	scales = rand(2, n)
-	opacities = rand(1, n)
-	return GSplatData(means, rots, colors, scales, opacities) 
-end
-
-splatData = genSplatData(25)
-splats = splatData
-idx = 1
-
-gt = load(joinpath(
-	ENV["HOME"],
-	"Desktop",
-	"six.png"
-))
-
-img = ones(RGBA{N0f8}, (400, 400))
-cimgview = channelview(img)
-cgtview = channelview(gt)
 
 function drawSplat!(cov2d, quadView, point, color, opacity, TPrev, αPrev)
 	sz = size(quadView)
@@ -80,6 +57,7 @@ function renderSplats(splats, cimage)
 		Δ = det(cov2d)
 
 		if Δ < 0.0
+			@warn "Determinant is negative"
 			continue
 		end
 		
@@ -98,8 +76,10 @@ function renderSplats(splats, cimage)
 		drawSplat!(cov2d, quadView, point, color, opacity, TPrev, αPrev)
 	end
 		# backward
-		return image
-	for idx in nPoints:1
+	S = zeros(eltype(image), size(image))
+	grads = []
+	image = deepcopy(image)
+	for idx in nPoints:-1:1
 		point = [size(cimage)[2:3]...].*splats.means[:, idx]
 		rot = reshape(splats.rotations[:, idx], (2, 2))
 		scale = Diagonal(splats.scales[:, idx])
@@ -110,6 +90,7 @@ function renderSplats(splats, cimage)
 		Δ = det(cov2d)
 
 		if Δ < 0.0
+			@warn "Determinant is negative"
 			continue
 		end
 		
@@ -127,21 +108,60 @@ function renderSplats(splats, cimage)
 		idxs = CartesianIndices(Tuple([1:sz[1], 1:sz[2]])) |> collect
 		αs = ones(N0f8, sz...)
 		invCov2d = inv(cov2d)
-		for idx in idxs
-			delta = [idx.I[1] - point[1], idx.I[2] - point[2]]./[sz...]
+		Δo = zeros(Float32, size(quadView))
+		Δσ = zeros(Float32, size(quadView))
+		Δμ = zeros(Float32, 2, sz...)
+		ΔΣ = zeros(Float32, 2, 2, sz...)
+		for pidx in idxs
+			delta = [pidx.I[1] - point[1], pidx.I[2] - point[2]]./[sz...]
 			dist = (delta |> adjoint)*invCov2d*delta
+			Δμ[:, pidx] .= invCov2d*delta
+			ΔΣ[:, :, pidx] .= -0.5.*(invCov2d*delta*(delta |> adjoint)*(invCov2d |> adjoint))
 			α = opacity*exp(-dist)
-			αs[idx] = α |> N0f8
+			αs[pidx] = α |> N0f8
+			Δo[pidx] = exp(-dist)
+			Δσ[pidx] = -opacity*exp(-dist)
 		end
+		Δo = permuteddimsview(repeat(Δo, 1, 1, 4), (3, 1, 2))
+		Δσ = permuteddimsview(repeat(Δσ, 1, 1, 4), (3, 1, 2))
 		# calculate gradients for colors of splats
-		Δc = αs.*TPrev
-		# calculate Transmittance online
+		TPrev = view(transmittance, UnitRange(bb[1, :]...), UnitRange(bb[2, :]...))
+		Δc = permuteddimsview(repeat(αs.*TPrev, 1, 1, 4), (3, 1, 2))
+		# calculate gradients for α of splats
+		SPrev = view(S, UnitRange(bb[1, :]...), UnitRange(bb[2, :]...))
+		Δα = (colorview(
+				RGBA, 
+				repeat([color[1]] .|> N0f8, inner=sz).*TPrev, 
+				repeat([color[2]] .|> N0f8, inner=sz).*TPrev, 
+				repeat([color[3]] .|> N0f8, inner=sz).*TPrev, 
+				ones(N0f8, sz)
+			) |> channelview) .- ((SPrev.*n0f8.(clamp.(1.0./(1.0 .- αs), 0.0, 1.0))) |> channelview)
+
+		grad = (ΔC) -> begin
+			cGrad = sum(Δc.*view(ΔC, :, UnitRange(bb[1, :]...), UnitRange(bb[2, :]...)), dims=(2, 3))[:][1:3]
+			#αGrad = sum(Δα.*view(ΔC, :, UnitRange(bb[1, :]...), UnitRange(bb[2, :]...)))
+			αGrad = Δα.*Δc.*view(ΔC, :, UnitRange(bb[1, :]...), UnitRange(bb[2, :]...))
+			oGrad = sum(Δo.*αGrad)
+			σGrad = sum(Δσ.*αGrad)
+			#μGrad = map((_idx) -> Δσ[_idx]*grads1, 1:size()
+			μGrad = sum(Δμ.*σGrad, dims=(2, 3))[:]
+			ΣGrad = sum(ΔΣ.*σGrad, dims=(3, 4))[:, :]
+			return (cGrad, oGrad, μGrad, ΣGrad)
+		end
+		push!(grads, grad)
+		# update S
+		SPrev .+= colorview(
+			RGBA, 
+			repeat([color[1]] .|> N0f8, inner=sz).*αs.*TPrev, 
+			repeat([color[2]] .|> N0f8, inner=sz).*αs.*TPrev, 
+			repeat([color[3]] .|> N0f8, inner=sz).*αs.*TPrev, 
+			ones(N0f8, sz)
+		)
+		# update Transmittance online
 		TPrev .= TPrev.*n0f8.(clamp.(1.0./(1.0 .- αs), 0.0, 1.0))
 	end
-	return image
+	return (image, grads)
 end
-
-outimage = renderSplats(splatData, cimgview)
 
 function forward(splatData, cimgview, gt)
 	outimage = renderSplats(splatData, cimgview)
@@ -153,8 +173,63 @@ function error(img, gt)
 end
 
 function errorGrad(img, gt)
-	s = error(img, gt)
-	return -(1.0 .-img .- gt)/prod(size(img))
+	cimgview = channelview(img)
+	gtview = channelview(gt)
+	s = error(cimgview, gtview)
+	@info "loss : " s
+	return -(1.0 .-cimgview .- gtview)/prod(size(cimgview))
 end
 
+n = 10
+
+staticRot = rand(4, n);
+staticMeans = rand(2, n)
+#staticOpacities = ones(1, n)
+staticScales = ones(2, n)
+staticColors = rand(3, n)
+
+function genSplatData(n)
+	means = staticMeans
+	rots = staticRot#rand(4, n) 
+	colors = rand(3, n)
+	scales = staticScales # rand(4, n)
+	opacities = rand(1, n)
+	return GSplatData(means, rots, colors, scales, opacities) 
+end
+
+splatDataOriginal = genSplatData(n)
+
+imgSize = (64, 64)
+img = ones(RGBA{N0f8}, imgSize)
+cimgview = channelview(img)
+
+(outimage, grads) = renderSplats(splatDataOriginal, cimgview)
 save("fontsplat.jpg", outimage)
+
+
+gt = load("fontsplat.jpg")
+gt = colorview(RGBA, gt, ones(N0f8, size(gt)))
+cgtview = channelview(gt)
+
+splatData = genSplatData(n)
+(outimage, grads) = renderSplats(splatData, cimgview)
+
+lr = -0.011
+for i in 1:1000
+	@info "iteration: $(i)"
+	img = ones(RGBA{N0f8}, imgSize)
+	cimgview = channelview(img)
+	(outimage, grads) = renderSplats(splatData, cimgview)
+	println("saving fontsplat $(i)")
+	save("fontsplat$(i).jpg", outimage)
+	ΔC = errorGrad(outimage, gt)
+	nPoints = size(splatData.means, 2)
+	for idx in nPoints:-1:1
+		(cGrad, oGrad, μGrad, ΣGrad) = grads[idx](ΔC)
+		#splatData.means[:, idx] .+= lr.*μGrad
+		splatData.colors[:, idx] .+= lr.*cGrad
+		splatData.opacities[:, idx] .+= lr.*oGrad
+	end
+end
+
+
